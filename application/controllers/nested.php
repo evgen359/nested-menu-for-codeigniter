d50b8a079ba359361e9e152db89eb87da67fa59e
n1crack <numberone.fatalrhymer@gmail.com>
2012-06-11 00:14:31 +0300

    several changes has been made..

--- application/controllers/nested.php
+++ application/controllers/nested.php
@@ -23,6 +23,22 @@ class Nested extends CI_Controller {
 		$this->load->view('nested', $data);
 	}
 	
+	public function get_json()
+	{
+		$this->load->model('MPTtree');
+		$this->MPTtree->set_table('sections');
+		
+		$root = $this->MPTtree->get_root();
+		$this->db->select('descendants.id, descendants.lft, descendants.rgt, descendants.title, Count(pages.id) as num_pages, depth');
+		$this->MPTtree->AR_from_alldescendants_of($root['lft'],$root['rgt']);
+		$this->db->join('pages','descendants.id = pages.cat_id', 'left outer');
+		$this->db->group_by('descendants.id');
+		$this->db->order_by('descendants.lft');
+		$query = $this->db->get();
+		$rows = $query->result_array();
+		echo json_encode($rows);
+	}	
+	
 	public function ajax_sort()
 	{		
      	$item = $this->input->post('item');	
@@ -39,5 +55,26 @@ class Nested extends CI_Controller {
      	  $this->MPTtree->move_node_after($node['lft'], $snode['lft']);
 		 
 	}
+	
+	public function ajax_addnew()
+	{
+										
+		$newdata=array("title" => $this->input->post('title'));	
+		$this->load->model('MPTtree');
+		$this->MPTtree->set_table('sections'); 
+		$this->MPTtree->append_node_last(1, $newdata); 
+		echo $this->input->post('title');
 
+	}
+	public function ajax_delete()
+	{
+										
+		$this->load->model('MPTtree');
+		$this->MPTtree->set_table('sections'); 
+		$this->MPTtree->set_debug(TRUE);
+		$this->MPTtree->delete_node($this->input->post('lft'));
+		echo "OK";
+
+	}	
+	
 }
--- application/views/nested.php
+++ application/views/nested.php
@@ -9,6 +9,15 @@
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.ui.nestedSortable.js"></script>
 
 		<style type="text/css">
+
+		body { font-size: 62.5%; }
+		#dialog-form label,#dialog-form  input { display:block; }
+		 input.text { margin-bottom:12px; width:95%;	}
+			fieldset { padding:0; border:0;	 }
+		 h1 { font-size: 1.1px; margin: .6em 0; }
+		.ui-dialog .ui-state-error { padding: .3em; }
+		.validateTips { border: 1px solid transparent; padding: 0.3em; }
+		
 		.placeholder {
 			background-color: #cfcfcf;
 		}
@@ -20,6 +29,7 @@
 			margin: 0;
 			padding: 0;
 			padding-left: 30px;
+			font-size: 14px;
 		}
 		ol.sortable, ol.sortable ol {
 			margin: 0 0 0 25px;
@@ -34,28 +44,84 @@
 			padding: 0;
 		}
 
-		.sortable li div  {
+		.sortable li div	{
 			border: 1px solid black;
 			padding: 3px;
 			margin: 0;
 			/*cursor: move;*/
 		}
-		.sortable li div span  {
+		.sortable li div span	 {
 			cursor: pointer;
 		}	
+		.deletebutton {
+			float:right;position:relative;right:0px;top:0px;display:block;width:4px;height:4px;margin:0;padding:0;background-image:url('http://localhost:8888/ci/assets/img/Delete.gif');border: 0px solid transparent !important;cursor:pointer
+		}
 		</style>	
 	</head>
 	<body>
 		<script>
+
+
+		$(document).ready(function() {
 		
-		$(function makeNested() {
+		var name = $( "#name" )
+		var bNested = false;
+		refresh_menu();
+		
+		// Add button
+		$( "#addnew" )
+			.button()
+			.click(function() {$( "#dialog-form" ).dialog( "open" );
+		});
+		
+		// Edit button
+		$( "#edit" )
+			.button()
+			.click(function() {
+				if (bNested == false){
+					bNested = true;	
+					$("span", this).text("edit: off");		
+				} else {
+					bNested = false;
+					$("span", this).text("edit: on");
+				}
+				refresh_menu();
+		});
+		
+		// dialog form
+		$( "#dialog-form" ).dialog({
+			autoOpen: false,
+			height: 175,
+			width: 350,
+			modal: true,
+			buttons: {
+				"Add new section": function() {
+			        $.ajax({
+			        	url: "<?php echo base_url(); ?>index.php/nested/ajax_addnew",
+			        	type: "POST",
+			        	data: ({ title : name.val()}),
+			        	success: function(data){refresh_menu();}
+			        });	
+					refresh_menu();
+					$(this).dialog( "close" );
+				},
+				Cancel: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {}
+		});
+				
+		// Making nested 
+		function makeNested() { 
+			$('ol.sortable').nestedSortable('destroy');
 			$('ol.sortable').nestedSortable({
 				disableNesting: 'no-nest',
 				forcePlaceholderSize: true,
 				handle: 'span.handle',
 				helper:	'clone',
 				items: 'li',
-				maxLevels: 3,
+				maxLevels: 4,
 				opacity: .6,
 				placeholder: 'placeholder',
 				revert: 250,
@@ -64,73 +130,102 @@
 				toleranceElement: '> div',
 				
 				update : function (event, ui)  {
-		         var result = $('ol.sortable').nestedSortable('toArray');
-		         var item = ui.item;//.attr('id');
-		   
-		         var moveto = "";
-		         var type = "";
-		         
-			         if ($(item).index() != 0)
-			         {
-				        moveto = $(item).prev().attr("id");
-				        type = 'addnext';
-			         } 
-			         else 
-			         {
-					    moveto =($($(item).parents().get(0)).attr("id") == "root") ? "1" : $($(item).parents().get(1)).attr("id") ;
-				        type = 'appendfirst';
-			         }
-			        			        
+				   var result = $('ol.sortable').nestedSortable('toArray');
+				   var item = ui.item;
+				   var moveto = "";
+				   var type = "";
+				   
+						if ($(item).index() != 0){
+						moveto = $(item).prev().attr("id");
+						type = 'addnext';
+						} else {
+						moveto =($($(item).parents().get(0)).attr("id") == "root") ? "1" : $($(item).parents().get(1)).attr("id") ;
+						type = 'appendfirst';
+						}
+					   					  
 					$.ajax({
 					  url: "<?php echo base_url(); ?>index.php/nested/ajax_sort",
 					  type: "POST",
 					  data: ({ item : $(item).attr("id").match(/[0-9]+/g)[0], moveto : moveto.match(/[0-9]+/g)[0], type : type}),
-					  success: function(data){ 
-					  }
+					  success: function(data){refresh_menu();}
 					 });
-		         }
+				   }
+				   
 			});
-
-			$('#makeitdisable').click(function(){
-				$('ol.sortable').nestedSortable('destroy');
-				$('span.handle').hide();
-			})
+			}
 			
-			$('#makeitenable').click(function(){
-				makeNested();
-				$('span.handle').show();
-			})
-
+	
+			// getting Json and parsing menu..
+			function refresh_menu() { 
+			 $.ajax({
+				 url: "<?php echo base_url(); ?>index.php/nested/get_json",
+			  type: "POST",
+			  data: ({ title : $("#t_addnew").val()}),
+			  success: function(data){ 
+			   var items = $.parseJSON(data);
+			   var k = 1, c = items.length, j, nestedhtml = '';
+			  	$.each(items, function(i,item){ // 
+			  	 if(item.depth == j)	 {nestedhtml += '</li>';}
+			  	 else if(parseInt(item.depth) > j){nestedhtml += '<ol>';}
+			  	 else if(parseInt(item.depth) < j){nestedhtml += Array(Math.abs(parseInt(item.depth)-parseInt(j+1))).join('</li></ol>') + "</li>";}	
+				 nestedhtml += '<li id="item_' + item.id + '" class="ui-state-default"><div><span class="handle" ><a class="ui-icon ui-icon-grip-dotted-vertical" style="float:left;"></a></span>' + item.title + '<small> (' + item.num_pages + ' item)</small><span class="lft" id="'+item.lft+'"></span></div>';
+				 if(k == c) { nestedhtml += Array(Math.abs(parseInt(item.depth)-1)).join('</li></ol>') + "</li>" };
+				 k++;
+				 j = parseInt(item.depth);
+				 });
+				 $('#root').html(nestedhtml);
+				 $("<div class='deletebutton'></div>").appendTo("#root.sortable li div");
+				 $(".deletebutton").hide();
+				 
+				 if (bNested == false){
+					$(".handle").attr("style","display:none");
+				 }else{
+					$('#root.sortable li div').bind({
+					 mouseenter: function() {$(this).find(".deletebutton").show();},
+					 mouseleave: function() {$(this).find(".deletebutton").hide();},});	
+					 makeNested();
+				 }
+				 
+				 $('.deletebutton').bind('click', function() {
+					if (confirm("Are you sure to delete this item ?")==true)
+					{	
+  				 	  $.ajax({
+					  	 url: "<?php echo base_url(); ?>index.php/nested/ajax_delete",
+					  	 type: "POST",
+					  	 data: ({ lft : $(this).parent().find("span.lft").attr("id")}),
+					  	 success: function(data){}
+					  });	
+					  refresh_menu();
+					}
+				  });
+			  }
+			 });	
+			}
+						
 		});
 		</script>
-			<input type="submit" name="makeitdisable" id="makeitdisable" value="Disable Sorting" />
-			<input type="submit" name="makeitdenable" id="makeitenable" value="Enable Sorting" />
+			<button id="edit">edit: on</button>
+			<button id="addnew">add new item</button>
 
 		<div style="width:480px;">
 		<ol class="sortable" id="root">
-		<?php  
-		$menu = $sections;
-		 $i = 1;
-		 $active_menu = '';
-		 $menu_items = count($menu);
-		 foreach($menu as $item)
-		 {
-			$item = (object)$item;
-		    if(isset($j) && $item->depth == $j)    echo "</li>";
-		    elseif(isset($j) && $item->depth > $j) echo "<ol>";
-		    elseif(isset($j) && $item->depth < $j) echo str_repeat('</li></ol>', abs($item->depth-$j)) . "</li>";
-
-		    echo '<li id="item_'.$item->id.'"  class="ui-state-default"><div><span class="handle"><a class="ui-icon ui-icon-grip-dotted-vertical" style="float:left;"></a></span>'. $item->title . '<small> (' . $item->num_pages . ' item)</small></div>';
-
-		    if($i == $menu_items) echo str_repeat('</li></ol>', abs($item->depth-1)) . "</li>";
-
-		    $i++;
-		    $j = $item->depth;
-		    $k = $item->lft;
-		 }
-		?>
+		
 		</ol>
-		</div>
+		</div>		
+
+<div id="dialog-form" title="Create new section">
+	<p class="validateTips">All form fields are required.</p>
+
+	<form>
+	<fieldset>
+		<label for="name">Title</label>
+		<input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />
+	</fieldset>
+	</form>
+</div>
+
+
+		
 	</body>
 </html>
 
